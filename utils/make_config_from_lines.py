42ac765c11063f050e4583f431a8571a37f82829
Mariusz Glebocki <mglebocki@antmicro.com>
2020-10-18 02:08:24 +0200

    utils: Remove leftover print
    Signed-off-by: Mariusz Glebocki <mglebocki@antmicro.com>

--- utils/make_config_from_lines.py
+++ utils/make_config_from_lines.py
@@ -7,7 +7,6 @@ obj = {"dataFilesList": []}
 for line in sys.stdin:
     line = line.rstrip()
     (name, url) = line.split("\0")
-    print({"name": name, "url": url}, file=sys.stderr)
     obj["dataFilesList"].append({"name": name, "url": url})
 
 json.dump(obj, sys.stdout, indent=1)
